93a516a7f26cd148c3cb197006887df83b12c36f
killua <killua@killua2.0>
2023-10-06 02:00:25 +0100

    low

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entrypoint
+ *
+ * Description: simple masmsoussi code print some aadab f alx using puts
+ *
+ * Return: 0 always succes like massmoussi
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
+
+puts
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry mass
+ *
+ * Description: C massmoussi code print somthing
+ *
+ * Return: 0 is massmoussi succes
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
