38e5d32b160dd8ac9ad15c29f5f39dd85911766f
killua <killua@killua2.0>
2023-10-13 02:41:57 +0100

    my debug test

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
-* print_remaining_days - get a date and prints how many days they are 
+* print_remaining_days - takes a date and prints how many days are
 * left in the year, taking leap years into consideration
 * @month: month in number format
 * @day: day of month
-* @year: 1 year
+* @year: year
 * Return: void
 */
 
