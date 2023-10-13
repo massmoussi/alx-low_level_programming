57d798617efb9aa48b41a6fe75c9deb3202c7df8
killua <killua@killua2.0>
2023-10-13 02:27:12 +0100

    my debug things

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - EntryPoint
+ *
+ * Return: 0 Always Success
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/3-print_remaining_days.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+* print_remaining_days - get a date and prints how many days they are 
+* left in the year, taking leap years into consideration
+* @month: month in number format
+* @day: day of month
+* @year: 1 year
+* Return: void
+*/
+
+void print_remaining_days(int month, int day, int year)
+{
+	int days_in_year;
+
+	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
+	{
+		/* leap year */
+		days_in_year = 366;
+		if (month > 2 && day >= 60)
+			day++;
+	}
+	else
+	{
+		/* not a leap year */
+		days_in_year = 365;
+		if (month == 2 && day == 60)
+		{
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+			return;
+		}
+	}
+
+	printf("Day of the year: %d\n", day);
+	printf("Remaining days: %d\n", days_in_year - day);
+}
