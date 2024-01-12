3d379c9284ba3f6bca61bf6cf372131c7949a445
killua <killua@killua2.0>
2024-01-12 18:02:45 +0100

    static l

---
+++ 0x09-static_libraries/create_static_lib.sh
@@ -0,0 +1,11 @@
+#!/bin/bash                                                                     
+
+#generate the Object file                                                       
+gcc -c *.c
+
+#Create liball.a                                                                
+ar rcs liball.a *.o
+
+#remove all the object file                                                     
+echo y | rm -I *.o
+
