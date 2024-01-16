a0893a5a7bc0b54d32378ed6c811d857adc11057
killua <killua@killua2.0>
2024-01-16 22:09:39 +0100

    update malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,40 +1,39 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * _strdup - function name
  *
- * Description: Function measures a variable & duplicates it
+ * Description: Function mesure a variable & duplicate it
  *
  * @str: string
  *
- * Return: Null if there is an issue, Otherwise The New String
+ * Return: Null if there is issue, otherwise the new string
  */
 
 char *_strdup(char *str)
 {
-    unsigned int i, j;
-    char *string;
+	unsigned int i, j;
+	char *string;
 
-    if (!str)
-    {
-        return (NULL);
-    }
+	if (!str)
+	{
+		return (NULL);
+	}
 
-    j = strlen(str);
+	j = strlen(str);
 
-    string = malloc((j + 1) * sizeof(char));
+	string = malloc((j + 1) * sizeof(char));
 
-    if (!string)
-    {
-        return (NULL);
-    }
+	if (!string)
+	{
+		return (NULL);
+	}
 
-    for (i = 0; i < j; i++)
-    {
-        string[i] = str[i];
-    }
-    string[i] = '\0';
+	for (i = 0; i < j; i++)
+	{
+		string[i] = str[i];
+	}
+	string[i] = '\0';
 
-    return (string);
+	return (string);
 }
