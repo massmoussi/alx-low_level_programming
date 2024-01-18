10bf665e19e28b5dcbea8b42c2f9602ca0c37472
killua <killua@killua2.0>
2024-01-19 00:54:40 +0100

    update more malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,20 +3,21 @@
 /**
  * malloc_checked - function check if the memory is allocated or not
  *
- * @b: bytes of memory To allocate
+ * @b: bytes of memory to allocate
  *
- * Return: it's just a void
+ * Return: it's a void
  */
+
 void *malloc_checked(unsigned int b)
 {
-    int *p;
+	int *p;
 
-    p = malloc(b);
+	p = malloc(b);
 
-    if (!p)
-    {
-        exit(98);
-    }
+	if (!p)
+	{
+		exit(98);
+	}
 
-    return (p);
+	return (p);
 }
