72101422fae6a1e4f779fed8f3caeffff468f939
killua <killua@killua2.0>
2024-01-26 00:10:37 +0100

    variadec fun fun

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdarg.h>
-
+#include <variadic_functions.h>
 /**
  * main - entry var function
  *
@@ -8,17 +8,20 @@
  *
  * return: 0 lik always
  */
-int sum_them_all(const unsigned int n, ...){
+int sum_them_all(const unsigned int n, ...)
+{
 	if (n == 0)
-		return 0;
+		return (0);
 	int sum = 0;
 	var_list args;
+
 	var_start(args, n);
 
-	for (int i =0; i < n; i++){
-	sum += var_arg(args,int);
+	for (int i = 0; i < n; i++)
+	{
+	sum += var_arg(args, int);
 	}
 	var_end(args);
-	return sum;
+	return (sum);
 }
 
