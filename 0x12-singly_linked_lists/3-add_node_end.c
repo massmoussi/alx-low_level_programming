d930e1e9dc1ecb27ef233961c467cd0d5f99b616
killua <killua@killua2.0>
2024-02-02 00:44:35 +0100

    fourth one 1

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -16,3 +16,27 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *N_node, *temp;
 
 	if (str == NULL)
+		l_str = 0;
+
+	l_str = strlen(str);
+
+	N_node = malloc(sizeof(list_t));
+	if (N_node == NULL)
+		return (NULL);
+
+	N_node->str = strdup(str);
+	N_node->next = NULL;
+	N_node->len = l_str;
+
+	temp = *head;
+
+	if (temp == NULL)
+		*head = N_node;
+	else
+	{
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = N_node;
+	}
+			return (*head);
+}
